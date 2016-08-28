4df7c2cb0ffffb7191748660ce0fa5f2edf0b55a
Ancient Hunter <ancient.hunter@gmail.com>
2016-08-28 14:45:23 -0700

    UuuuOOOO

---
+++ CrosswordSolver/CrosswordSolver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrosswordSolver
+//
+//  Created by Prabhat Kiran on 8/28/16.
+//  Copyright (c) 2016 ___PrabhatKiranBharathidhasan___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
