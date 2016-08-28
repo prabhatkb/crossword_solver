0405dceb13a60c25956a49ccc0693eaf874388b6
prabhatkb <prabhat.bharathi@gmail.com>
2016-08-28 15:16:16 -0700

    Let's change the directory structure

---
+++ CrosswordSolverTests/CrosswordSolverTests.m
@@ -0,0 +1,34 @@
+//
+//  CrosswordSolverTests.m
+//  CrosswordSolverTests
+//
+//  Created by Prabhat Kiran on 8/28/16.
+//  Copyright (c) 2016 ___PrabhatKiranBharathidhasan___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrosswordSolverTests : XCTestCase
+
+@end
+
+@implementation CrosswordSolverTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
